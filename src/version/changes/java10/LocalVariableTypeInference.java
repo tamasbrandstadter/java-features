36072559bbed146fcaf5e07b9dcab5341ad78a7a
tamasbrandstadter <tamas.brandstadter@gmail.com>
2018-12-02 17:06:57 +0100

    add java 10 examples

---
+++ src/version/changes/java10/LocalVariableTypeInference.java
@@ -0,0 +1,35 @@
+package version.changes.java10;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalVariableTypeInference {
+
+    /* var can be used for:
+            - variables within a method
+            - resource variables for try-with-resources
+            - loop variable in for-each loop
+     */
+    public static void main(String[] args) throws IOException {
+        try (var input = new FileInputStream("test.txt")) {
+            System.out.println(input.getChannel().size());
+        }
+
+        // the compiler infers the type from the elements
+        var numbers = List.of(1, 2, 3, 4, 5);
+        assert numbers instanceof ArrayList;
+
+        // the compiler infers the type from collection passed
+        var numbers2 = new ArrayList<>(numbers);
+
+        // this will be an ArrayList<Object> because the compiler cannot infer a specific type
+        var numbers3 = new ArrayList<>();
+
+        for (var num : numbers) {
+            System.out.println(num);
+        }
+    }
+
+}
---
+++ src/version/changes/java10/UnmodifiableCollections.java
@@ -0,0 +1,33 @@
+package version.changes.java10;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnmodifiableCollections {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        // java.util.List, java.util.Map and java.util.Set each got a new static method copyOf(Collection).
+        // It returns the unmodifiable copy of the given Collection
+        // Any attempt to modify such a collection would result in java.lang.UnsupportedOperationException runtime exception.
+        List<Integer> copy = List.copyOf(numbers);
+
+        // java.util.stream.Collectors get additional methods to collect a Stream into unmodifiable List, Map or Set
+        // Any attempt to modify such a collection would result in java.lang.UnsupportedOperationException runtime exception.
+        List<Integer> evenNumbers = numbers.stream()
+            .filter(i -> i % 2 == 0)
+            .collect(Collectors.toUnmodifiableList());
+
+        // java.util.Optional, java.util.OptionalDouble, java.util.OptionalInt and java.util.OptionalLong each got a new method orElseThrow()
+        // which doesn’t take any argument and throws NoSuchElementException if no value is present.
+        // It’s synonymous with and is now the preferred alternative to the existing get()method.
+        Integer firstEven = numbers.stream()
+            .filter(i -> i % 2 == 0)
+            .findFirst()
+            .orElseThrow();
+
+
+    }
+
+}
